7d823af352fee7a4a48bd25e4b062e558743c49e
Robson Ferreira <tech.rob@icloud.com>
2021-06-10 19:02:36 -0400

    second commit

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 from marshmallow import fields
-from marshmallow_sqlalchemy import ModelSchema
+from flask_marshmallow import Marshmallow
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+
 
 # instance of the Flask application, setup database config and create db object
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost:3306/movie'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:mysqlpassmacrob@localhost:3306/movie'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 
 # create movie model
@@ -31,8 +35,8 @@ class Movie(db.Model):
 db.create_all()
 
 # the movie schema makes it possible to return JSON from the Python objects from SQLALCHEMY
-class MovieSchema(ModelSchema):
-    class Meta(ModelSchema.Meta):
+class MovieSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
         model = Movie
         sqla_session = db.session
     id = fields.Number(dump_only=True)
@@ -42,17 +46,51 @@ class MovieSchema(ModelSchema):
 
 # creating the endpoints
 
-@app.route('api/v1/movie', methods=['POST'])
-def post_movie():
+# route that posts a movie
+@app.route('/api/v1/movie', methods=['POST'])
+def create_movie():
     data = request.get_json()
     movie_schema = MovieSchema()
     movie = movie_schema.load(data)
     result = movie_schema.dump(movie.create())
-    return make_response(jsonify({ 'movie': result}), 200)
+    return make_response(jsonify({"movie": result}), 200)
 
-@app.route('api/v1/movie', methods=['GET'])
+# route that returns a list of movies
+@app.route('/api/v1/movie', methods=['GET'])
 def index():
     get_movies = Movie.query.all()
     movie_schema = MovieSchema(many=True)
     movies = movie_schema.dump(get_movies)
     return make_response(jsonify({ 'movies': movies}))
+
+# route that retrieves a movie by passing a specific id
+@app.route('/api/v1/movie/<id>', methods=['GET'])
+def get_movie_by_id(id):
+    get_movie = Movie.query.get(id)
+    movie_schema = MovieSchema()
+    movie = movie_schema.dump(get_movie)
+    return make_response(jsonify({ 'movie': movie}))
+
+# update a movie by passing a specific id
+@app.route('/api/v1/movie/<id>', methods=['PUT'])
+def update_movie_by_id(id):
+    data = request.get_json()
+    get_movie = Movie.query.get(id)
+    if data.get('title'):
+        get_movie.title = data['title']
+    if data.get('movie_description'):
+        get_movie.movie_description = data['movie_description']
+    db.session.add(get_movie)
+    db.session.commit()
+    movie_schema = MovieSchema(only=['id', 'title', 'movie_description'])
+    movie = movie_schema.dump(get_movie)
+    
+    return make_response(jsonify({ 'movie': movie}))
+
+# delete a movie by a specific id
+@app.route('/api/v1/movie/<id>', methods=['DELETE'])
+def delete_movie_by_id(id):
+    get_movie = Movie.query.get(id)
+    db.session.delete(get_movie)
+    db.session.commit()
+    return make_response("", 204)
